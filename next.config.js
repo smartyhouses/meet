88b0657ee00701717420cabb286baaa8d08df72d
smartyhouses <108938542+smartyhouses@users.noreply.github.com>
2025-08-06 23:30:12 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: false,
   productionBrowserSourceMaps: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     formats: ['image/webp'],
   },
